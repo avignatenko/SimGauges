3c0a814017873cc4e6d171ccc2b4019b7c4dd9bc
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2021-12-22 02:05:35 +0300

    extracted basic instrument & Altimeter instrument

--- src/Altimeter/main.cpp
+++ src/Altimeter/main.cpp
@@ -3,6 +3,7 @@
 
 #include "TaskAltimeterMenu.h"
 
+#include <BasicInstrument.h>
 #include <TaskButton.h>
 #include <TaskCAN.h>
 #include <TaskErrorLed.h>
@@ -14,13 +15,11 @@
 
 #include <GyverFilters.h>
 
-Scheduler taskManager;
-
 // hardware speficics
-const int BUTTON_PORT = 7;
-const int LED_PORT = 3;
-const int MCP2515_SPI_PORT = 10;
-const int MCP2515_INT_PIN = 2;
+const byte BUTTON_PORT = 7;
+const byte LED_PORT = 3;
+const byte MCP2515_SPI_PORT = 10;
+const byte MCP2515_INT_PIN = 2;
 
 const int STEPPER_STEP = A0;
 const int STEPPER_DIR = A1;
@@ -30,35 +29,10 @@ const int kEEPROMAddrIndex = 0;
 const int kEEPROMCal0Index = kEEPROMAddrIndex + sizeof(uint16_t);
 const int kEEPROMLUTIndex = 5;
 
-void initSerial()
-{
-    Serial.begin(9600);
-    while (!Serial)
-        ;
-}
-
-void onButtonPressed(bool pressed)
-{
-    if (pressed)
-        TaskErrorLed::instance().addError(TaskErrorLed::ERROR_TEST_LED);
-    else
-        TaskErrorLed::instance().removeError(TaskErrorLed::ERROR_TEST_LED);
-}
-
 StoredLUT<kEEPROMLUTIndex> s_lut;
 
-void onSetValue(byte priority, byte port, uint16_t srcAddress, uint16_t dstAddress, byte len, byte* payload, void* data)
-{
-    if (len != 4)
-        {
-            return;
-        }
+/*
 
-    float pos = *reinterpret_cast<float*>(payload);
-
-    TaskStepperTMC2208::instance().setPosition(
-        static_cast<uint16_t>(Interpolation::ConstrainedSpline(s_lut.x(), s_lut.y(), s_lut.size(), pos)));
-}
 
 void onPosCommand(int32_t pos)
 {
@@ -111,152 +85,225 @@ void onCal0VarSet(float value)
     int32_t cal0 = (int32_t)value;
     EEPROM.put(kEEPROMCal0Index, cal0);
 }
+*/
+// void loopTest2()
+//{
+//    pinMode(A5, INPUT);
+//    int resRaw = analogRead(A5);
+//    // Serial.println(resRaw);
+//}
 
-void loopTest2()
+class TaskCalibrate : public Task
 {
-    pinMode(A5, INPUT);
-    int resRaw = analogRead(A5);
-    // Serial.println(resRaw);
-}
-void loopKnob()
-{
-    pinMode(A4, INPUT);
-    int resRaw = analogRead(A4);
-
-    //Serial.println(resRaw);
-
-    // 31.0 -> 0
-    // 28 -> 820
-
-    float p = (resRaw - 0) * (28.0 - 31.0) / (820 - 0) + 31.0;
-
-    float p0 = 29.92;
-
-    // https://en.wikipedia.org/wiki/Pressure_altitude
-    float h = 145366.45 * (1 - pow(p / p0, 0.190284));
-
-    int posRaw = h * 16 * 200 / 1000;
-
-    // exp filter
-    static GFilterRA filter(0.01);
-    int pos = filter.filtered(posRaw);
-
-    TaskStepperTMC2208::instance().setPosition(pos);
-}
-Task taskKnob(1 * TASK_MILLISECOND, TASK_FOREVER, &loopKnob, &taskManager, false);
-
-bool initSensor();
-
-Task taskTest(TASK_IMMEDIATE, TASK_FOREVER, nullptr, &taskManager, false, initSensor);
-Task taskTest2(500 * TASK_MILLISECOND, TASK_FOREVER, &loopTest2, &taskManager, true);
-
-float readSensorFiltered()
-{
-    int res_raw = analogRead(A3);
-    static GFilterRA filter(0.8);
-    int res = filter.filtered(res_raw);
-
-    return res * 5.0 / 1023.0;
-}
-
-void sensorFinalize()
-{
-    if (TaskStepperTMC2208::instance().position() == TaskStepperTMC2208::instance().targetPosition())
+public:
+    TaskCalibrate(TaskStepperTMC2208& stepper, Task& taskKnob, unsigned long aInterval = 0, long aIterations = 0,
+                  Scheduler* aScheduler = NULL, bool aEnable = false)
+        : Task(aInterval, aIterations, aScheduler, aEnable), stepper_(stepper), taskKnob_(taskKnob)
     {
-        TaskStepperTMC2208::instance().resetPosition(0);
-        taskTest.disable();
-        taskKnob.enable();
     }
-}
 
-void sensorFinalizeInit()
+    virtual bool Callback() override { return (this->*callback_)(); }
+
+private:
+    bool initSensor()
+    {
+        Serial.println("initSensor");
+        pinMode(A3, INPUT);
+
+        callback_ = &TaskCalibrate::sensorMoveAwayFromMarkerInit;
+
+        return true;
+    }
+
+    float readSensorFiltered()
+    {
+        int res_raw = analogRead(A3);
+        static GFilterRA filter(0.8);
+        int res = filter.filtered(res_raw);
+
+        return res * 5.0 / 1023.0;
+    }
+
+    bool sensorFinalize()
+    {
+        if (stepper_.position() == stepper_.targetPosition())
+        {
+            stepper_.resetPosition(0);
+            disable();
+            taskKnob_.enable();
+        }
+
+        return true;
+    }
+
+    bool sensorFinalizeInit()
+    {
+        Serial.println("sensorFinalizeInit");
+        // 10863
+        stepper_.setPosition(stepper_.position() + 10863);  // onCal0VarGet());
+
+        callback_ = &TaskCalibrate::sensorFinalize;
+
+        return true;
+    }
+
+    bool sensorFindMarkerFast()
+    {
+        if (readSensorFiltered() > 2) callback_ = &TaskCalibrate::sensorFinalizeInit;
+
+        return true;
+    }
+
+    bool sensorFindMarkerFastInit()
+    {
+        Serial.println("sensorFindMarkerFastInit");
+        stepper_.setPosition(stepper_.position() - 200L * 10 * 10 * 16);  // full circle
+        callback_ = &TaskCalibrate::sensorFindMarkerFast;
+
+        return true;
+    }
+
+    bool sensorMoveAwayFromMarker()
+    {
+        if (readSensorFiltered() < 1) callback_ = &TaskCalibrate::sensorFindMarkerFastInit;
+
+        return true;
+    }
+
+    bool sensorMoveAwayFromMarkerInit()
+    {
+        Serial.println("sensorMoveAwayFromMarkerInit");
+        stepper_.setPosition(stepper_.position() - 200L * 10 * 10 * 16);  // full circle
+
+        callback_ = &TaskCalibrate::sensorMoveAwayFromMarker;
+
+        return true;
+    }
+
+private:
+    TaskStepperTMC2208& stepper_;
+    Task& taskKnob_;
+    bool (TaskCalibrate::*callback_)() = &TaskCalibrate::initSensor;
+};
+
+class TaskKnob : public Task
 {
-    Serial.println("sensorFinalizeInit");
-    // 10863
-    TaskStepperTMC2208::instance().setPosition(TaskStepperTMC2208::instance().position() + onCal0VarGet());
-    taskTest.setCallback(sensorFinalize);
-}
+public:
+    TaskKnob(TaskStepperTMC2208& stepper, unsigned long aInterval = 0, long aIterations = 0,
+             Scheduler* aScheduler = NULL, bool aEnable = false)
+        : Task(aInterval, aIterations, aScheduler, aEnable), stepper_(stepper)
+    {
+    }
 
-void sensorFindMarkerFast()
+    virtual bool Callback() override
+    {
+        pinMode(A4, INPUT);
+        int resRaw = analogRead(A4);
+
+        // Serial.println(resRaw);
+
+        // 31.0 -> 0
+        // 28 -> 820
+
+        float p = (resRaw - 0) * (28.0 - 31.0) / (820 - 0) + 31.0;
+
+        float p0 = 29.92;
+
+        // https://en.wikipedia.org/wiki/Pressure_altitude
+        float h = 145366.45 * (1 - pow(p / p0, 0.190284));
+
+        int posRaw = h * 16 * 200 / 1000;
+
+        // exp filter
+        static GFilterRA filter(0.01);
+        int pos = filter.filtered(posRaw);
+
+        stepper_.setPosition(pos);
+
+        return true;
+    }
+
+private:
+    TaskStepperTMC2208& stepper_;
+};
+
+class Altimeter : public BasicInstrument
 {
-    if (readSensorFiltered() > 2) taskTest.setCallback(sensorFinalizeInit);
-}
+public:
+    Altimeter(byte ledPin, byte buttonPin, byte canSPIPin, byte canIntPin)
+        : BasicInstrument(ledPin, buttonPin, canSPIPin, canIntPin),
+          taskStepper_(taskManager_, STEPPER_STEP, STEPPER_DIR, STEPPER_RESET, 100, 100),
+          taskKnob_(taskStepper_, 1 * TASK_MILLISECOND, TASK_FOREVER, &taskManager_, false),
+          taskCalibrate_(taskStepper_, taskKnob_, TASK_IMMEDIATE, TASK_FOREVER, &taskManager_, false)
+    {
+        taskCAN_.setReceiveCallback(fastdelegate::MakeDelegate(this, &Altimeter::onSetValue));
+    }
 
-void sensorFindMarkerFastInit()
-{
-    Serial.println("sensorFindMarkerFastInit");
-    TaskStepperTMC2208::instance().setPosition(TaskStepperTMC2208::instance().position() -
-                                               200L * 10 * 10 * 16);  // full circle
-    taskTest.setCallback(sensorFindMarkerFast);
-}
+    void setup()
+    {
+        BasicInstrument::setup();
 
-void sensorMoveAwayFromMarker()
-{
-    if (readSensorFiltered() < 1) taskTest.setCallback(sensorFindMarkerFastInit);
-}
+        Serial.println("Altimeter::setup()");
+        taskStepper_.start();
+        taskCalibrate_.enable();
+        /*
+                TaskAltimeterMenu::init(taskManager_);
+                TaskAltimeterMenu::instance().setPosCallback(onPosCommand);
+                TaskAltimeterMenu::instance().setInteractiveCallback(onInteractiveCommand);
+                TaskAltimeterMenu::instance().addVar("addr", onAddrVarGet, onAddrVarSet);
+                TaskAltimeterMenu::instance().addVar("cal0", onCal0VarGet, onCal0VarSet);
 
-void sensorMoveAwayFromMarkerInit()
-{
-    Serial.println("sensorMoveAwayFromMarkerInit");
-    TaskStepperTMC2208::instance().setPosition(TaskStepperTMC2208::instance().position() -
-                                               200L * 10 * 10 * 16);  // full circle
-    taskTest.setCallback(sensorMoveAwayFromMarker);
-}
+                // port 0: set arrow position
+                taskCAN_.setReceiveCallback(onSetValue, 0);*/
+        /*
+                // set some fake LUT
+                bool hasLut = s_lut.load();
 
-bool initSensor()
-{
-    Serial.println("initSensor");
-    pinMode(A3, INPUT);
-    taskTest.setCallback(sensorMoveAwayFromMarkerInit);
-    taskTest.enable();
+                if (!hasLut)
+                {
+                    s_lut.clear();
 
-    return true;
-}
+                    s_lut.addValue(0.0, 0.0);
+                    s_lut.addValue(40, 380);
+                    s_lut.addValue(60, 928);
+                    s_lut.addValue(80, 1417);
+                    s_lut.addValue(100, 1869);
+                    s_lut.addValue(200, 3657);
+                }
+
+               */
+        // TaskMenu::instance().start();
+    }
+
+private:
+    void onSetValue(byte priority, byte port, uint16_t srcAddress, uint16_t dstAddress, byte len, byte* payload)
+    {
+        if (len != 4)
+        {
+            return;
+        }
+
+        float pos = *reinterpret_cast<float*>(payload);
+
+        // TaskStepperTMC2208::instance().setPosition(
+        //    static_cast<uint16_t>(Interpolation::ConstrainedSpline(s_lut.x(), s_lut.y(), s_lut.size(), pos)));
+    }
+
+private:
+    TaskStepperTMC2208 taskStepper_;
+    TaskKnob taskKnob_;
+    TaskCalibrate taskCalibrate_;
+};
+
+Altimeter s_instrument(LED_PORT, BUTTON_PORT, MCP2515_SPI_PORT, MCP2515_INT_PIN);
 
 void setup()
 {
-    initSerial();
-
-    TaskErrorLed::init(taskManager, LED_PORT);
-    TaskStepperTMC2208::init(taskManager, STEPPER_STEP, STEPPER_DIR, STEPPER_RESET, 100, 100);
-    TaskButton::init(taskManager, BUTTON_PORT);
-    TaskButton::instance().setPressedCallback(onButtonPressed);
-    TaskAltimeterMenu::init(taskManager);
-    TaskAltimeterMenu::instance().setPosCallback(onPosCommand);
-    TaskAltimeterMenu::instance().setInteractiveCallback(onInteractiveCommand);
-    TaskMenu::instance().addVar("addr", onAddrVarGet, onAddrVarSet);
-    TaskMenu::instance().addVar("cal0", onCal0VarGet, onCal0VarSet);
-
-    TaskCAN::init(taskManager, MCP2515_SPI_PORT, MCP2515_INT_PIN, onAddrVarGet());
-
-    // port 0: set arrow position
-    TaskCAN::instance().setReceiveCallback(onSetValue, 0);
-
-    // set some fake LUT
-    bool hasLut = s_lut.load();
-
-    if (!hasLut)
-    {
-        s_lut.clear();
-
-        s_lut.addValue(0.0, 0.0);
-        s_lut.addValue(40, 380);
-        s_lut.addValue(60, 928);
-        s_lut.addValue(80, 1417);
-        s_lut.addValue(100, 1869);
-        s_lut.addValue(200, 3657);
-    }
-
-    TaskErrorLed::instance().start();
-    TaskStepperTMC2208::instance().start();
-    TaskButton::instance().start();
-    TaskCAN::instance().start();
-    TaskMenu::instance().start();
-
-    taskTest.enable();
+    Serial.begin(9600);
+    s_instrument.setup();
 }
 
 void loop()
 {
-    taskManager.execute();
+    s_instrument.run();
 }
