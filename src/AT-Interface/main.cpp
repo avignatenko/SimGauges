3cc4fe76219a1e40392c9c20f2541f3f60b428b2
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2022-05-24 02:53:12 +0300

    (in progress) updates for AT gauge

---
+++ src/AT-Interface/main.cpp
@@ -0,0 +1,207 @@
+
+#include <Common.h>
+
+#include <BasicInstrument.h>
+#include <Interpolation.h>
+#include <TaskButton.h>
+#include <TaskCAN.h>
+#include <TaskErrorLed.h>
+
+#include <Wire.h>
+
+// hardware speficics
+const byte BUTTON_PORT = 7;
+const byte LED_PORT = 3;
+const byte MCP2515_SPI_PORT = 10;
+const byte MCP2515_INT_PIN = 2;
+
+struct MotorUpdate
+{
+    uint8_t calibration;  // 0 - not calibrated, 1 - in progress, 2 - calibrated
+    int16_t posCard;
+    int16_t posBug;
+    int8_t direction1;
+    long millis1;
+    int8_t direction2;
+    long millis2;
+};
+
+class TaskI2CMaster : private Task
+{
+public:
+    TaskI2CMaster(Scheduler* aScheduler = NULL) : Task(10 * TASK_MILLISECOND, TASK_FOREVER, aScheduler, false)
+    {
+        instance_ = this;
+    }
+
+    virtual bool Callback() override { return true; }
+
+    void setPosition(byte motor, int16_t position)
+    {
+        Serial.print("A");
+        Wire.beginTransmission(2);
+        Serial.print("A");
+        Wire.write((uint8_t)motor);
+        Serial.print("A");
+        Wire.write(reinterpret_cast<uint8_t*>(&position), sizeof(position));
+        Serial.print("A");
+        Wire.endTransmission();
+        Serial.print("A");
+        motorPos_[motor] = position;
+        Serial.print("A");
+    }
+
+    int16_t position(byte motor) const { return motorPos_[motor]; }
+
+    void requestUpdate() { Wire.requestFrom(2, sizeof(MotorUpdate)); }
+
+    using Task::enable;
+
+protected:
+    virtual bool OnEnable() override
+    {
+        Serial.print("Wire ininitiazed");
+        Wire.begin(1);  // Activate I2C network
+        Wire.onReceive(&TaskI2CMaster::receiveEvent);
+        Serial.print("Wire ininitiazed");
+        return true;
+    }
+
+    virtual void OnDisable() override {}
+
+private:
+    static void receiveEvent(int howMany)
+    {
+        if (Wire.available() < sizeof(MotorUpdate)) return;
+
+        TaskI2CMaster* me = TaskI2CMaster::instance_;
+
+        Wire.readBytes((uint8_t*)&me->m_, sizeof(MotorUpdate));
+    }
+
+private:
+    static TaskI2CMaster* instance_;
+    int16_t motorPos_[2] = {0};
+
+    volatile MotorUpdate m_;
+};
+
+TaskI2CMaster* TaskI2CMaster::instance_ = nullptr;
+
+class GenericSingleNeedleInstrument : public BasicInstrument
+{
+public:
+    GenericSingleNeedleInstrument(byte ledPin, byte buttonPin, byte canSPIPin, byte canIntPin)
+        : BasicInstrument(ledPin, buttonPin, canSPIPin, canIntPin), taskI2C_(&taskManager_)
+    {
+        posCard_ = addPos("card");
+        posBug_ = addPos("bug");
+
+        varCardCal_ = addVar("cardcal");
+        varBugCal_ = addVar("bugcal");
+    }
+
+    void setup()
+    {
+        BasicInstrument::setup();
+        taskI2C_.enable();
+    }
+
+protected:
+    virtual void setVar(byte idx, float value) override { BasicInstrument::setVar(idx, value); }
+
+    virtual int32_t posForLut(byte idxLut) override { return taskI2C_.position(idxLut); }
+    virtual int32_t pos(byte idxPos) override { return taskI2C_.position(idxPos == posCard_ ? 0 : 1); }
+
+    virtual void setPos(byte idx, int32_t value, bool absolute) override
+    {
+        BasicInstrument::setPos(idx, value, absolute);
+
+        int32_t pos = (absolute ? value : taskI2C_.position(idx) + value);
+        taskI2C_.setPosition(idx == posCard_ ? 0 : 1, pos);
+    }
+
+    int16_t angleToSteps(float angle) { return static_cast<int16_t>(angle * 12); }
+    float stepsToAngle(int16_t steps) { return static_cast<float>(steps) / 12; }
+
+    float lPos(byte idx)
+    {
+        uint8_t curPosIdx = (idx == posCard_ ? 0 : 1);
+        int16_t offset = getVar(idx == posCard_ ? varCardCal_ : varBugCal_);
+
+        float curLPos = stepsToAngle(taskI2C_.position(curPosIdx) - offset);
+
+        if (idx == posBug_)
+        {
+            float lPosCard = lPos(posCard_);
+            curLPos += lPosCard;
+        }
+        return curLPos;
+    }
+
+    void setLPosInternal(byte idx, float value)
+    {
+        if (idx == posBug_)
+        {
+            float lPosCard = lPos(posCard_);
+            value -= lPosCard;
+        }
+
+        uint8_t curPosIdx = (idx == posCard_ ? 0 : 1);
+        int16_t offset = getVar(idx == posCard_ ? varCardCal_ : varBugCal_);
+
+        int16_t newPos = angleToSteps(value) + offset;
+        int16_t posDelta = newPos - taskI2C_.position(curPosIdx);
+        taskI2C_.setPosition(curPosIdx, newPos);
+
+        if (idx == posCard_)
+        {
+            taskI2C_.setPosition(1, taskI2C_.position(1) - posDelta);
+        }
+    }
+
+    virtual void setLPos(byte idx, float value, bool absolute) override
+    {
+        value = fmod(value, 360);
+
+        BasicInstrument::setLPos(idx, value, absolute);
+
+        // get current angle and calculate delta
+        float curLPos = lPos(idx);
+        float delta = fmod(value - curLPos + 540, 360) - 180;
+        float newValue = curLPos + delta;
+
+        setLPosInternal(idx, newValue);
+    }
+
+private:
+    virtual void onCANReceived(byte priority, byte port, uint16_t srcAddress, uint16_t dstAddress, byte len,
+                               byte* payload) override
+    {
+        if (len != 4) return;
+
+        float pos = *reinterpret_cast<float*>(payload);
+        setLPos(port, pos, true);
+    }
+
+private:
+    TaskI2CMaster taskI2C_;
+    byte posCard_;
+    byte posBug_;
+
+    byte varCardCal_;
+    byte varBugCal_;
+};
+
+GenericSingleNeedleInstrument s_instrument(LED_PORT, BUTTON_PORT, MCP2515_SPI_PORT, MCP2515_INT_PIN);
+
+void setup()
+{
+    Serial.begin(9600);
+    s_instrument.setup();
+}
+
+void loop()
+{
+    s_instrument.run();
+}
---
+++ src/AT-Motors/main.cpp
@@ -0,0 +1,296 @@
+
+#include <Common.h>
+
+#include <BasicInstrument.h>
+#include <Interpolation.h>
+
+#include <TaskEncoder.h>
+#include <TaskStepperTMC2208.h>
+
+#include <Wire.h>
+// hardware speficics
+
+const byte STEPPER_A_STEP = A0;
+const byte STEPPER_A_DIR = A1;
+
+const byte STEPPER_B_STEP = A2;
+const byte STEPPER_B_DIR = A3;
+
+const byte STEPPER_RESET = 13;
+
+struct MotorUpdate
+{
+    uint8_t calibration;  // 0 - not calibrated, 1 - in progress, 2 - calibrated
+    int16_t posCard;
+    int16_t posBug;
+    int8_t direction1;
+    long millis1;
+    int8_t direction2;
+    long millis2;
+};
+
+class TaskI2CSlave : private Task
+{
+public:
+    TaskI2CSlave(Scheduler* aScheduler = NULL) : Task(TASK_IMMEDIATE, TASK_ONCE, aScheduler, false)
+    {
+        instance_ = this;
+    }
+
+    virtual bool Callback() override {}
+
+    void start() { enable(); }
+
+    void sendUpdate(const MotorUpdate& update)
+    {
+        Wire.beginTransmission(1);
+        Wire.write(reinterpret_cast<const uint8_t*>(&update), sizeof(MotorUpdate));
+
+        Wire.endTransmission();
+    }
+
+    void sendStatusUpdate() {}
+
+    void setReceiveCallback(fastdelegate::FastDelegate2<int8_t, int16_t> callback) { receiveCallback_ = callback; }
+
+protected:
+    virtual bool OnEnable() override
+    {
+        Wire.begin(2);  // Activate I2C network
+        Wire.onReceive(&TaskI2CSlave::receiveEvent);
+        Wire.onRequest(&TaskI2CSlave::onRequest);
+        return true;
+    }
+
+    virtual void OnDisable() override {}
+
+private:
+    static void onRequest() {}
+
+    static void receiveEvent(int howMany)
+    {
+        if (Wire.available() < sizeof(byte) + sizeof(int16_t)) return;
+
+        TaskI2CSlave* me = TaskI2CSlave::instance_;
+        byte motor = Wire.read();
+        int16_t pos = 0;
+        Wire.readBytes(reinterpret_cast<uint8_t*>(&pos), sizeof(pos));
+
+        if (me->receiveCallback_) me->receiveCallback_(motor, pos);
+    }
+
+private:
+    fastdelegate::FastDelegate2<int8_t, int16_t> receiveCallback_;
+
+    static TaskI2CSlave* instance_;
+};
+
+TaskI2CSlave* TaskI2CSlave::instance_ = nullptr;
+
+/*
+class TaskCalibrate : private Task
+{
+public:
+    TaskCalibrate(TaskStepperX27Driver& stepperCard, TaskStepperX27Driver& stepperBug, Scheduler* aScheduler = NULL,
+                  bool aEnable = false)
+        : Task(TASK_IMMEDIATE, TASK_FOREVER, aScheduler, aEnable), stepperCard_(stepperCard), stepperBug_(stepperBug)
+    {
+    }
+
+    virtual bool Callback() override { return (this->*callback_)(); }
+
+    void start() { enable(); }
+
+    int status() { return status_; }
+
+    void setFinishPosition(int8_t motor, int16_t position) { finishPos_[motor] = position; }
+
+protected:
+    virtual bool OnEnable() override { return true; }
+
+    virtual void OnDisable() override {}
+
+private:
+    bool initResetToWhite()
+    {
+        Serial.println("initResetToWhite");
+        stepperCard_.resetPosition(0);
+        stepperCard_.setPosition(360 * 12 * 1.2);
+
+        stepperBug_.resetPosition(0);
+        stepperBug_.setPosition(360 * 12 * 1.2);
+
+        callback_ = &TaskCalibrate::resetToWhite;
+        return true;
+    }
+
+    bool resetToWhite()
+    {
+        status_ = 1;
+        // Serial.println("resetToWhite");
+        int calRead = analogRead(A6);
+        // Serial.println(calRead);
+        if (calRead < 100)
+        {
+            stepperCard_.stop();
+            stepperBug_.stop();
+            callback_ = &TaskCalibrate::initCalibrateCard;
+        }
+
+        return true;
+    }
+
+    bool initCalibrateCard()
+    {
+        Serial.println("initCalibrateCard");
+        stepperCard_.resetPosition(0);
+        stepperCard_.setPosition(360 * 12 * 1.2);
+
+        callback_ = &TaskCalibrate::calibrateCard;
+
+        return true;
+    }
+    bool calibrateCard()
+    {
+        // Serial.println("calibrateCard");
+        int calRead = analogRead(A6);
+        // Serial.println(calRead);
+        if (calRead > 200)
+        {
+            Serial.println("calibrateCard found pos, moving to +60 degrees");
+            Serial.println(stepperCard_.position());
+            stepperCard_.setPosition(stepperCard_.position() + 60 * 12);
+            callback_ = &TaskCalibrate::finishCalibrateCard;
+        }
+        return true;
+    }
+
+    bool finishCalibrateCard()
+    {
+        if (stepperCard_.position() == stepperCard_.targetPosition())
+        {
+            callback_ = &TaskCalibrate::initCalibrateBug;
+        }
+
+        return true;
+    }
+
+    bool initCalibrateBug()
+    {
+        Serial.println("initCalibrateBug");
+        stepperBug_.resetPosition(0);
+        stepperBug_.setPosition(360 * 12 * 1.2);
+
+        callback_ = &TaskCalibrate::calibrateBug;
+
+        return true;
+    }
+
+    bool calibrateBug()
+    {
+        // Serial.println("calibrateBug");
+        int calRead = analogRead(A6);
+        // Serial.println(calRead);
+        if (calRead > 200)
+        {
+            Serial.println("calibrateBug found pos");
+            Serial.println(stepperBug_.position());
+            stepperBug_.stop();
+            callback_ = &TaskCalibrate::finishCalibrateBug;
+        }
+        return true;
+    }
+
+    bool finishCalibrateBug()
+    {
+        if (stepperBug_.position() == stepperBug_.targetPosition())
+        {
+            stepperBug_.resetPosition(finishPos_[1]);
+            stepperCard_.resetPosition(finishPos_[0]);
+
+            status_ = 2;
+            disable();
+        }
+
+        return true;
+    }
+
+private:
+    bool (TaskCalibrate::*callback_)() = &TaskCalibrate::initResetToWhite;
+    TaskStepperX27Driver& stepperCard_;
+    TaskStepperX27Driver& stepperBug_;
+    int status_ = 0;
+    int16_t finishPos_[2] = {0, 0};
+};
+*/
+
+class ATMotors : public InstrumentBase
+{
+public:
+    ATMotors()
+        : taskStepperBug_(taskManager_, STEPPER_A_STEP, STEPPER_A_DIR, STEPPER_RESET),
+          taskStepperCard_(taskManager_, STEPPER_B_STEP, STEPPER_B_DIR, STEPPER_RESET),
+          //         taskCalibrate_(taskStepperCard_, taskStepperBug_, &taskManager_),
+          taskI2C_(&taskManager_)
+    {
+        taskI2C_.setReceiveCallback(fastdelegate::FastDelegate2<int8_t, int16_t>(this, &ATMotors::onReceive));
+     //   taskStepperBug_.setMaxSpeed(1000);
+     //   taskStepperBug_.setMaxAcceleration(3000);
+
+     //   taskStepperCard_.setMaxSpeed(1000);
+     //   taskStepperCard_.setMaxAcceleration(3000);
+
+    }
+
+    void setup()
+    {
+        taskStepperBug_.start();
+        taskStepperCard_.start();
+
+         //     taskCalibrate_.start();
+
+        taskI2C_.start();
+
+        Serial.println("AA");
+
+        taskStepperBug_.setPosition(-5000);
+      taskStepperCard_.setPosition(1000);
+
+      Serial.println("AA");
+    }
+
+private:
+   
+    void onReceive(int8_t motor, int16_t pos)
+    {
+        //      if (taskCalibrate_.status() < 2)
+        //      {
+        //          taskCalibrate_.setFinishPosition(motor, pos);
+        //          return;
+        //      }
+
+        if (motor == 0)
+            taskStepperCard_.setPosition(pos);
+        else if (motor == 1)
+            taskStepperBug_.setPosition(pos);
+    }
+
+private:
+    TaskStepperTMC2208 taskStepperBug_;
+    TaskStepperTMC2208 taskStepperCard_;
+    //   TaskCalibrate taskCalibrate_;
+    TaskI2CSlave taskI2C_;
+};
+
+ATMotors s_instrument;
+
+void setup()
+{
+    Serial.begin(9600);
+    s_instrument.setup();
+}
+
+void loop()
+{
+    s_instrument.run();
+}
