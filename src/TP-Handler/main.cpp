d101bdc594386120571ceca24b34738ecc80b7bb
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2023-03-05 16:21:00 +0300

    Added support for MCP23017 in transponder

--- src/TP-Handler/main.cpp
+++ src/TP-Handler/main.cpp
@@ -1,17 +1,19 @@
 
+#include "Bounce2MCP23017.h"
+
 #include <Common.h>
 
 #include <BasicInstrument.h>
 
+#include <Adafruit_MCP23X17.h>
 #include <LedControl_SW_SPI.h>
-#include <TaskButton.h>
+#include <TaskButtonMCP23017.h>
 #include <TaskEncoder.h>
 
-#include <Wire.h>
-// hardware speficics
+//  hardware speficics
 
 const uint8_t AP_SELECTOR_PORTS_NUM = 5;
-const uint8_t AP_SELECTOR_PORTS[AP_SELECTOR_PORTS_NUM] = {2, 3, 4, 5, 6};
+const uint8_t AP_SELECTOR_PORTS[AP_SELECTOR_PORTS_NUM] = {0, 1, 2, 3, 4};
 const uint8_t ENC_1_CW_PORT = 4;
 const uint8_t ENC_1_CCW_PORT = 3;
 
@@ -28,18 +30,12 @@ const uint8_t DIGITS_DATA_PIN = 9;
 const uint8_t DIGITS_CLK_PIN = 7;
 const uint8_t DIGITS_CS_PIN = 8;
 
+Adafruit_MCP23X17 mcp;
+
 class SelectorTask : private Task
 {
 public:
-    SelectorTask(Scheduler& sh) : Task(20 * TASK_MILLISECOND, TASK_FOREVER, &sh, true)
-    {
-        for (int i = 0; i < AP_SELECTOR_PORTS_NUM; ++i)
-        {
-            apSelector_[i].attach(AP_SELECTOR_PORTS[i], INPUT_PULLUP);
-            apSelector_[i].interval(20);
-            apSelector_[i].setPressedState(LOW);
-        }
-    }
+    SelectorTask(Scheduler& sh) : Task(20 * TASK_MILLISECOND, TASK_FOREVER, &sh, true) {}
 
     void setSelectorCallback(fastdelegate::FastDelegate1<int8_t> callback) { apSelectorCallback_ = callback; }
 
@@ -55,7 +51,16 @@ public:
         return 0;
     }
 
-    void start() { enable(); }
+    void start()
+    {
+        for (int i = 0; i < AP_SELECTOR_PORTS_NUM; ++i)
+        {
+            apSelector_[i].attach(AP_SELECTOR_PORTS[i], INPUT_PULLUP);
+            apSelector_[i].interval(20);
+            apSelector_[i].setPressedState(LOW);
+        }
+        enable();
+    }
 
 private:
     bool Callback() override
@@ -80,7 +85,9 @@ private:
     }
 
 private:
-    Bounce2::Button apSelector_[AP_SELECTOR_PORTS_NUM];
+    Bounce2::ButtonMCP23017 apSelector_[AP_SELECTOR_PORTS_NUM] = {
+        Bounce2::ButtonMCP23017(mcp), Bounce2::ButtonMCP23017(mcp), Bounce2::ButtonMCP23017(mcp),
+        Bounce2::ButtonMCP23017(mcp), Bounce2::ButtonMCP23017(mcp)};
     fastdelegate::FastDelegate1<int8_t> apSelectorCallback_;
 };
 
@@ -140,98 +147,47 @@ private:
     bool on_ = false;
 };
 
-class TaskI2CSlave : private Task
-{
-public:
-    TaskI2CSlave(Scheduler* aScheduler = NULL) : Task(TASK_IMMEDIATE, TASK_ONCE, aScheduler, false)
-    {
-        instance_ = this;
-    }
-
-    virtual bool Callback() override { return false; }
-
-    void start() { enable(); }
-
-    /*
-        void sendUpdate(const MotorUpdate& update)
-        {
-            Wire.beginTransmission(1);
-            //        Wire.write(reinterpret_cast<const uint8_t*>(&update), sizeof(MotorUpdate));
-
-            Wire.endTransmission();
-        }
-    */
-    void sendStatusUpdate() {}
-
-    void setReceiveCallback(fastdelegate::FastDelegate2<int8_t, int16_t> callback) { receiveCallback_ = callback; }
-
-protected:
-    virtual bool OnEnable() override
-    {
-        Wire.begin(2);  // Activate I2C network
-        Wire.onReceive(&TaskI2CSlave::receiveEvent);
-        Wire.onRequest(&TaskI2CSlave::onRequest);
-        return true;
-    }
-
-    virtual void OnDisable() override {}
-
-private:
-    static void onRequest() {}
-
-    static void receiveEvent(int howMany)
-    {
-        if (Wire.available() < sizeof(byte) + sizeof(int16_t)) return;
-
-        TaskI2CSlave* me = TaskI2CSlave::instance_;
-        byte motor = Wire.read();
-        int16_t pos = 0;
-        Wire.readBytes(reinterpret_cast<uint8_t*>(&pos), sizeof(pos));
-
-        if (me->receiveCallback_) me->receiveCallback_(motor, pos);
-    }
-
-private:
-    fastdelegate::FastDelegate2<int8_t, int16_t> receiveCallback_;
-
-    static TaskI2CSlave* instance_;
-};
-
-TaskI2CSlave* TaskI2CSlave::instance_ = nullptr;
-
 class ATMotors : public InstrumentBase
 {
 public:
     ATMotors()
-        :  // taskI2C_(&taskManager_),
-          taskDigits_(&taskManager_),
-          // taskButton_(taskManager_, 11),
-          // taskSelector_(taskManager_),
+        : taskDigits_(&taskManager_),
+          taskButton_(taskManager_, mcp, 6),
+          taskSelector_(taskManager_),
           taskEncoder1_(taskManager_, ENC_1_CW_PORT, ENC_1_CCW_PORT),
           taskEncoder2_(taskManager_, ENC_2_CW_PORT, ENC_2_CCW_PORT),
           taskEncoder3_(taskManager_, ENC_3_CW_PORT, ENC_3_CCW_PORT),
           taskEncoder4_(taskManager_, ENC_4_CW_PORT, ENC_4_CCW_PORT)
     {
-        //  taskI2C_.setReceiveCallback(fastdelegate::FastDelegate2<int8_t, int16_t>(this, &ATMotors::onReceive));
-        //  taskButton_.setPressedCallback(fastdelegate::FastDelegate2<bool, byte>(this, &ATMotors::onButtonPressed));
-        //  taskSelector_.setSelectorCallback(fastdelegate::FastDelegate1<int8_t>(this, &ATMotors::onSelectorSwitched));
+        taskButton_.setPressedCallback(fastdelegate::FastDelegate2<bool, byte>(this, &ATMotors::onButtonPressed));
+        taskSelector_.setSelectorCallback(fastdelegate::FastDelegate1<int8_t>(this, &ATMotors::onSelectorSwitched));
         taskEncoder1_.setRotationCallback(fastdelegate::FastDelegate2<int8_t, long>(this, &ATMotors::onEncoder1));
         taskEncoder2_.setRotationCallback(fastdelegate::FastDelegate2<int8_t, long>(this, &ATMotors::onEncoder2));
         taskEncoder3_.setRotationCallback(fastdelegate::FastDelegate2<int8_t, long>(this, &ATMotors::onEncoder3));
         taskEncoder4_.setRotationCallback(fastdelegate::FastDelegate2<int8_t, long>(this, &ATMotors::onEncoder4));
-        // analogWrite(10, 100);
     }
 
     void setup()
     {
-        // taskI2C_.start();
+        // init MCP first
+        if (!mcp.begin_I2C())
+        {
+            Serial.println("I2C Error.");
+        }
+
+        // configure LED pins for output
+        mcp.pinMode(5, OUTPUT);
+        mcp.digitalWrite(5, LOW);
+
+        mcp.pinMode(8, OUTPUT);
+        mcp.digitalWrite(8, LOW);
+
         taskDigits_.start();
         taskDigits_.setOn();  // temp
 
-        // taskButton_.start();
-        // taskSelector_.start();
+        taskButton_.start();
+        taskSelector_.start();
 
-        // Serial.print("enc start");
         taskEncoder1_.start();
         taskEncoder2_.start();
         taskEncoder3_.start();
@@ -246,30 +202,17 @@ private:
     uint8_t increment(uint8_t digit, int8_t dir)
     {
         int8_t newDigit = (int8_t)digit + dir;
-        return ((uint8_t)newDigit) % 8; 
+        return ((uint8_t)newDigit) % 8;
     }
-    void onEncoder1(int8_t dir, long)
-    {
-        taskDigits_.setDigit(0, increment(taskDigits_.digit(0), dir));
-    }
-    void onEncoder2(int8_t dir, long)
-    {
-        taskDigits_.setDigit(1, increment(taskDigits_.digit(1), dir));
-    }
-    void onEncoder3(int8_t dir, long)
-    {
-        taskDigits_.setDigit(3, increment(taskDigits_.digit(3), dir));
-    }
-    void onEncoder4(int8_t dir, long)
-    {
-        taskDigits_.setDigit(2, increment(taskDigits_.digit(2), dir));
-    }
+    void onEncoder1(int8_t dir, long) { taskDigits_.setDigit(0, increment(taskDigits_.digit(0), dir)); }
+    void onEncoder2(int8_t dir, long) { taskDigits_.setDigit(1, increment(taskDigits_.digit(1), dir)); }
+    void onEncoder3(int8_t dir, long) { taskDigits_.setDigit(3, increment(taskDigits_.digit(3), dir)); }
+    void onEncoder4(int8_t dir, long) { taskDigits_.setDigit(2, increment(taskDigits_.digit(2), dir)); }
 
 private:
-    // TaskI2CSlave taskI2C_;
     Digits taskDigits_;
-    // TaskButton taskButton_;
-    // SelectorTask taskSelector_;
+    TaskButtonMCP23017 taskButton_;
+    SelectorTask taskSelector_;
     TaskEncoder taskEncoder1_;
     TaskEncoder taskEncoder2_;
     TaskEncoder taskEncoder3_;
